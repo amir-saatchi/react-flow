94eed0750c1b051135253178074e17ce73f26baf
Amir <amir saatchi>
2025-03-14 14:22:08 +0330

    sound app started

--- components/SoundApp.tsx
+++ components/SoundApp.tsx
@@ -1,9 +1,29 @@
-'use client'
+"use client";
 
 function SoundApp() {
+  const context = new AudioContext();
+  const osc = context.createOscillator();
+  const amp = context.createGain();
+
+  osc.connect(amp);
+  amp.connect(context.destination);
+
+  osc.start();
+
+  const updateValues = (e) => {
+    const freq = (e.clientX / window.innerWidth) * 1000;
+    const gain = e.clientY / window.innerHeight;
+
+    osc.frequency.value = freq;
+    amp.gain.value = gain;
+  };
+
   return (
-    <div>SoundApp</div>
-  )
+    <div
+      className="h-dvh w-dvw"
+      onMouseMove={updateValues}
+    />
+  );
 }
 
-export default SoundApp
+export default SoundApp;
